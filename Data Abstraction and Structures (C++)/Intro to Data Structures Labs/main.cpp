8f5d7a61868b8b4e4d55bcecb9f7698b602ac18d
Michael Blackmon <michaeljblackmon@yahoo.com>
2023-10-02 12:45:00 -0700

    first lab

---
+++ Data Abstraction and Structures (C++)/Intro to Data Structures Labs/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include "LabPrinter.h"
+using namespace std;
+
+void CallFunctionNamed(LabPrinter &printer, string functionName)
+{
+   if (functionName == "Print2Plus2")
+   {
+      printer.Print2Plus2();
+   }
+   else if (functionName == "PrintSecret")
+   {
+      printer.PrintSecret();
+   }
+   else
+   {
+      cout << "Unknown function: " << functionName << endl;
+   }
+}
+int main(int argc, char *argv[])
+{
+   LabPrinter printer("abc");
+
+   CallFunctionNamed(printer, "Print2Plus2");
+   CallFunctionNamed(printer, "PrintPlus2");
+   CallFunctionNamed(printer, "PrintSecret");
+
+   return 0;
+}
